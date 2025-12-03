9599f52e41d196bf2d8c4953c072013ebf5ccb8e
Gustavo Mota <gustavo_mota094@outlook.com>
2025-12-02 22:36:05 -0300

    Small fixes

--- cars/urls.py
+++ cars/urls.py
@@ -16,6 +16,6 @@ urlpatterns = [
     path('car/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
     path('add-car/', CarCreateView.as_view(), name='add_car'),
     path('maintenance_record/<int:pk>', MaintenanceRecordView.as_view(), name='maintenance_record'),
-    path('add-maintenance_record/', AddMaintenanceRecordView.as_view(), name='add_maintenance_record'),
+    path('add-maintenance_record/', AddMaintenanceRecordView.as_view(), name='add-maintenance_record'),
     path('car/<int:pk>/delete/', CarDeleteView.as_view(), name='delete_car'),
 ]
